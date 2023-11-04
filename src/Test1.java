8a0c24118a4bfad584b1855b9220831fb79a439c
Medha Sreenidhi Kidambi <mkidamb@data.cs.purdue.edu>
2023-11-03 22:01:26 -0400

    Changing to if-else

--- src/Test1.java
+++ src/Test1.java
@@ -9,24 +9,18 @@ public class Test1 {
     public static double basicCalculator(final Operation operation, final int num1, final int num2){
         double result = 0;
 
-        switch(operation) {
-            case ADD:
-                // if operation = ADD, return the sum
+            if (operation == Operation.ADD) {
                 result = num1 + num2;
-                break;
-            case SUBTRACT:
+            } else if (operation == Operation.SUBTRACT) {
                 result = num1 - num2;
-                break;
-            case MULTIPLY:
+	    } else if (operation == Operation.MULTIPLY) {
                 result = num1 * num2;
-                break;
-            case DIVIDE:
+            } else if (operation == Operation.DIVIDE) {
                 if (num2 == 0) {
                     System.out.println("Can't divide by 0");
                 } else {
                     result = ((double)num1) / num2;
                 }
-                break;
         }
         return result;
     }
